cf93c3fe345a4f2d38ce4c14aaca778960a03d8d
Dharanikanna <dharanikanna1999@gmail.com>
2022-01-25 23:44:08 +0530

    Created environment and basic modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
+import './index.css';
+
+import Home from './home'
+import Healthcare from './healthcare'
+import Dashboard from './dashboard'
+import Form from './Form'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path ="/">
+          <Home />
+        </Route>
+    
+        <Route exact path ="/healthcare">
+          <Healthcare />
+        </Route>
+
+        <Route exact path ="/dashboard">
+          <Dashboard />
+        </Route>
+
+        <Route exact path ="/form">
+          <Form />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/healthcare.js
@@ -0,0 +1,9 @@
+function Health() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+  
+  export default Health;
